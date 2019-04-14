edaf307cbce00575bb0840958ce1e8ec95b8f28d
Catherinecql <qchen50@illinois.edu>
2019-04-14 18:13:43 -0500

    data cleaning

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # CS410_EventDetection
 
 Cleaner.py: 
+
+
+## The first step of data preparation
+### data_cleaning.py: remove urls, hashtags, stopwords, punctuation
+### 
--- data_cleaning.py
+++ data_cleaning.py
@@ -5,7 +5,7 @@ import pandas as pd
 import json
 import os
 import datetime
-from datetime import timedelta
+from datetime import datetime 
 from tqdm import tqdm
 import pickle
 from collections import OrderedDict
@@ -15,22 +15,115 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize  
 from nltk.util import ngrams
+from nltk.stem import PorterStemmer
+from nltk.stem import LancasterStemmer
 from collections import Counter
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 punctuations = ['…',',','.','!','?','/','~','|','"', '+',':', '(', ')', '*','-']
 stop_words = set(stopwords.words('english')) 
-
+porter = PorterStemmer()
+lancaster=LancasterStemmer()
+lancaster_dic = {}
 def read_all_files(num=1000):
-	fashion_file = pd.read_csv('./input/fashion_name_id_6k.csv',sep='\t')
-	# screen_names = list(fashion_file['screen_name'])
-	fashion_ids = list(fashion_file['id'])[0:1000]
-	print(fashion_ids)
+    file_dir = "./data/tweets_3200/"
+    fashion_file = pd.read_csv('./input/fashion_name_id_6k.csv',sep='\t')
+    fashion_ids = list(fashion_file['id'])[0:num]
+    for i, file_id in enumerate(fashion_ids):
+        print('read {}: {}'.format(i,file_id)) 
+        file = file_dir + str(file_id) + '.csv'
+        node_file = pd.read_csv(file, sep = ',')
+        tweets = list(node_file['text'])
+        cleaned_tweets, hashtags = clean_tweets(tweets)
+        new_dates = clean_dates(list(node_file['created_at']))
+        df = pd.DataFrame({'id': list(node_file['id']),
+                           'created_at': new_dates,
+                           'hashtags':hashtags,
+                           'cleaned_text': cleaned_tweets,
+                           # 'original_test':tweets
+                           })
+        df.to_csv("./result/clean_tweets/{}.csv".format(file_id), index=False, sep='\t')
 
+def clean_dates(dates):
+    new_dates = []
+    for date in dates:
+        date = datetime.strptime(date.split(' ')[0], '%Y-%m-%d').strftime('%Y-%m-%d')
+        new_dates.append(date)
+    return new_dates 
+def clean_tweets(tweets):
+    tweets = tweets
+    cleaned_text = []
+    hashtags = []
+    for i, sen in enumerate(tweets):
+        # print(sen)
+        sen = sen.split()
+        hashtag_item = []
+        if 'RT' == sen[0]:
+            if sen[1][0] == '@':
+                sen[0] = ''
+                sen[1] = ''
+        cleaned_tweet, hashtag_item = get_hashtag(sen)
+        cleaned_tweet = [element for element in cleaned_tweet if element != '' ]
+        cleaned_tweet = ' '.join(cleaned_tweet)
+        hashtags.append(hashtag_item)
+        cleaned_text.append(cleaned_tweet)
+    return cleaned_text, hashtags
+
+## extract and remove hashtags, 
+def get_hashtag(tweet):
+    hashtags = []
+    for i,word in enumerate(tweet):
+        temp_list = word.split('#')
+        if len(temp_list) > 1:
+            has = True
+            if temp_list[0] != '':
+                tweet[i] = temp_list[0]
+            else:
+                tweet[i] = ''
+            temp_list = temp_list[1:]
+            for hashtag in temp_list:                    
+                hashtag = word_preprocess(hashtag)
+                if hashtag != '': 
+                    hashtag =  '#' + hashtag.lower()
+                    if hashtag not in hashtags:
+                        hashtags.append(hashtag)
+        else:
+            tweet[i] = word_preprocess(word)
+    return tweet, hashtags
+
+## remove urls, stopwords, punctuations, 
+## lemmatize and stem each word
+def word_preprocess(word):
+    word = word.lower()
+    word = word.encode("ascii", errors="replace").decode()
+    if len(word) == 1 or (len(word) > 20 or word[0:4] == 'http'):
+        word = ''
+    for punct in punctuations:
+        word_list = [x for x in word.split(punct) if x]
+        if len(word_list) > 0:
+            word = word_list[0]
+        else:
+            word = ''
+    if word in stop_words:
+        word = ''
+    if word != '':
+        stem = lancaster.stem(word)
+        # add_lancaster_dic(stem, word)
+        word = stem
+    return word
+
+def add_lancaster_dic(stem, word):
+    if stem in lancaster_dic.keys():
+            if word not in lancaster_dic[stem]:
+                lancaster_dic[stem].append(word)
+    else:
+        lancaster_dic[stem] = [word]
+    print(lancaster_dic)   
 def main():
-	read_all_files()
-    # if not os.path.isdir('./data/vogue_list_tweets_analysis'):
-    #     os.mkdir('./data/vogue_list_tweets_analysis')
+    
+    if not os.path.isdir('./result/clean_tweets/'):
+        os.mkdir('./result/clean_tweets/')
+    read_all_files()
     # read_all_files()
     # read_a_file()
 
